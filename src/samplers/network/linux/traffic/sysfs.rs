cc95492e1bf6d0705f1cfcaa2297591b5779229c
Brian Martin <brayniac@gmail.com>
2024-06-11 13:45:58 -0700

    add fallback for net traffic sampler
    Adds a fallback network traffic sampler that reads from sysfs when
    BPF is disabled or not available.

--- src/samplers/network/linux/traffic/sysfs.rs
+++ src/samplers/network/linux/traffic/sysfs.rs
@@ -6,21 +6,20 @@ use metriken::Counter;
 use std::fs::File;
 use std::io::Read;
 use std::io::Seek;
+use super::NAME;
 
 #[distributed_slice(NETWORK_SAMPLERS)]
 fn init(config: &Config) -> Box<dyn Sampler> {
     let metrics = vec![
-        (&NETWORK_RX_CRC_ERRORS, "rx_crc_errors"),
-        (&NETWORK_RX_DROPPED, "rx_dropped"),
-        (&NETWORK_RX_MISSED_ERRORS, "rx_missed_errors"),
-        (&NETWORK_TX_DROPPED, "tx_dropped"),
+        (&NETWORK_RX_BYTES, "rx_bytes"),
+        (&NETWORK_RX_PACKETS, "rx_packets"),
+        (&NETWORK_TX_BYTES, "tx_bytes"),
+        (&NETWORK_TX_PACKETS, "tx_packets"),
     ];
 
-    if let Ok(s) = SysfsNetSampler::new(config, NAME, metrics) {
+    if let Ok(s) = SysfsNetSampler::new(config, super::NAME, metrics) {
         Box::new(s)
     } else {
         Box::new(Nop::new(config))
     }
 }
-
-const NAME: &str = "network_interfaces";
