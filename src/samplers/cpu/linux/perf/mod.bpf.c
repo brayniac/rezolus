48170e1cd69220ab8cae438c18c360b29c21741f
Brian Martin <brayniac@gmail.com>
2024-10-23 12:41:18 -0700

    allow getting counter fd

--- src/samplers/cpu/linux/perf/mod.bpf.c
+++ src/samplers/cpu/linux/perf/mod.bpf.c
@@ -6,6 +6,16 @@
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
 
+#define COUNTER_GROUP_WIDTH 8
+#define MAX_CPUS 1024
+
+// counter positions
+#define CYCLES 0
+#define INSTRUCTIONS 1
+#define TSC 2
+#define APERF 3
+#define MPERF 4
+
 // counters (see constants defined at top)
 struct {
 	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
