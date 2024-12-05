26ad9de875c29e8b5f140dbe4d242040683764ab
Brian Martin <brayniac@gmail.com>
2024-12-04 16:20:54 -0800

    avoid .as_any() for counters and gauges

--- src/exposition/http/mod.rs
+++ src/exposition/http/mod.rs
@@ -1,3 +1,4 @@
+use metriken::Value;
 use crate::common::HISTOGRAM_GROUPING_POWER;
 use crate::Arc;
 use crate::Config;
@@ -6,8 +7,6 @@ use axum::extract::State;
 use axum::routing::get;
 use axum::Router;
 use histogram::AtomicHistogram;
-use metriken::Counter;
-use metriken::Gauge;
 use metriken::RwLockHistogram;
 use tokio::net::TcpListener;
 use tower::ServiceBuilder;
@@ -107,13 +106,6 @@ async fn prometheus(State(state): State<Arc<AppState>>) -> String {
     let mut data = Vec::new();
 
     for metric in &metriken::metrics() {
-        let any = match metric.as_any() {
-            Some(any) => any,
-            None => {
-                continue;
-            }
-        };
-
         let name = metric.name();
 
         if name.starts_with("log_") {
@@ -124,73 +116,83 @@ async fn prometheus(State(state): State<Arc<AppState>>) -> String {
             continue;
         }
 
-        if let Some(counter) = any.downcast_ref::<Counter>() {
-            if metric.metadata().is_empty() {
+        match metric.value() {
+            Some(Value::Counter(value)) => {
+                let name_and_metadata = if metric.metadata().is_empty() {
+                    name
+                } else {
+                    &metric.formatted(metriken::Format::Prometheus)
+                };
+
                 data.push(format!(
-                    "# TYPE {name}_total counter\n{name}_total {} {timestamp}",
-                    counter.value()
-                ));
-            } else {
-                data.push(format!(
-                    "# TYPE {name} counter\n{} {} {timestamp}",
-                    metric.formatted(metriken::Format::Prometheus),
-                    counter.value()
+                    "# TYPE {name}_total counter\n{name_and_metadata}_total {value} {timestamp}",
                 ));
             }
-        } else if let Some(gauge) = any.downcast_ref::<Gauge>() {
-            data.push(format!(
-                "# TYPE {name} gauge\n{} {} {timestamp}",
-                metric.formatted(metriken::Format::Prometheus),
-                gauge.value()
-            ));
-        } else if let Some(histogram) = any.downcast_ref::<RwLockHistogram>() {
-            if state.config.prometheus().histograms() {
-                if let Some(histogram) = histogram.load() {
-                    let current = HISTOGRAM_GROUPING_POWER;
-                    let target = state.config.prometheus().histogram_grouping_power();
+            Some(Value::Gauge(value)) => {
+                let name_and_metadata = if metric.metadata().is_empty() {
+                    name
+                } else {
+                    &metric.formatted(metriken::Format::Prometheus)
+                };
 
-                    // downsample the histogram if necessary
-                    let downsampled: Option<histogram::Histogram> = if current == target {
-                        // the powers matched, we don't need to downsample
-                        None
-                    } else {
-                        Some(histogram.downsample(target).unwrap())
-                    };
+                data.push(format!(
+                    "# TYPE {name} gauge\n{name_and_metadata} {value} {timestamp}",
+                ));
+            }
+            Some(Value::Other(any)) => {
+                if let Some(histogram) = any.downcast_ref::<RwLockHistogram>() {
+                    if state.config.prometheus().histograms() {
+                        if let Some(histogram) = histogram.load() {
+                            let current = HISTOGRAM_GROUPING_POWER;
+                            let target = state.config.prometheus().histogram_grouping_power();
 
-                    // reassign to either use the downsampled histogram or the original
-                    let histogram = if let Some(histogram) = downsampled.as_ref() {
-                        histogram
-                    } else {
-                        &histogram
-                    };
+                            // downsample the histogram if necessary
+                            let downsampled: Option<histogram::Histogram> = if current == target {
+                                // the powers matched, we don't need to downsample
+                                None
+                            } else {
+                                Some(histogram.downsample(target).unwrap())
+                            };
 
-                    // we need to export a total count (free-running)
-                    let mut count = 0;
-                    // we also need to export a total sum of all observations
-                    // which is also free-running
-                    let mut sum = 0;
+                            // reassign to either use the downsampled histogram or the original
+                            let histogram = if let Some(histogram) = downsampled.as_ref() {
+                                histogram
+                            } else {
+                                &histogram
+                            };
 
-                    let mut entry = format!("# TYPE {name}_distribution histogram\n");
-                    for bucket in histogram {
-                        // add this bucket's sum of observations
-                        sum += bucket.count() * bucket.end();
+                            // we need to export a total count (free-running)
+                            let mut count = 0;
+                            // we also need to export a total sum of all observations
+                            // which is also free-running
+                            let mut sum = 0;
 
-                        // add the count to the aggregate
-                        count += bucket.count();
+                            let mut entry = format!("# TYPE {name}_distribution histogram\n");
+                            for bucket in histogram {
+                                // add this bucket's sum of observations
+                                sum += bucket.count() * bucket.end();
 
-                        entry += &format!(
-                            "{name}_distribution_bucket{{le=\"{}\"}} {count} {timestamp}\n",
-                            bucket.end()
-                        );
+                                // add the count to the aggregate
+                                count += bucket.count();
+
+                                entry += &format!(
+                                    "{name}_distribution_bucket{{le=\"{}\"}} {count} {timestamp}\n",
+                                    bucket.end()
+                                );
+                            }
+
+                            entry +=
+                                &format!("{name}_distribution_bucket{{le=\"+Inf\"}} {count} {timestamp}\n");
+                            entry += &format!("{name}_distribution_count {count} {timestamp}\n");
+                            entry += &format!("{name}_distribution_sum {sum} {timestamp}");
+
+                            data.push(entry);
+                        }
                     }
-
-                    entry +=
-                        &format!("{name}_distribution_bucket{{le=\"+Inf\"}} {count} {timestamp}\n");
-                    entry += &format!("{name}_distribution_count {count} {timestamp}\n");
-                    entry += &format!("{name}_distribution_sum {sum} {timestamp}");
-
-                    data.push(entry);
                 }
+            }
+            _ => {
+                continue;
             }
         }
     }
@@ -220,23 +222,22 @@ fn simple_stats(quoted: bool) -> Vec<String> {
     let q = if quoted { "\"" } else { "" };
 
     for metric in &metriken::metrics() {
-        let any = match metric.as_any() {
-            Some(any) => any,
-            None => {
-                continue;
-            }
-        };
-
         if metric.name().starts_with("log_") {
             continue;
         }
 
         let simple_name = metric.formatted(metriken::Format::Simple);
 
-        if let Some(counter) = any.downcast_ref::<Counter>() {
-            data.push(format!("{q}{simple_name}{q}: {}", counter.value()));
-        } else if let Some(gauge) = any.downcast_ref::<Gauge>() {
-            data.push(format!("{q}{simple_name}{q}: {}", gauge.value()));
+        match metric.value() {
+            Some(Value::Counter(value)) => {
+                data.push(format!("{q}{simple_name}{q}: {value}"));
+            }
+            Some(Value::Gauge(value)) => {
+                data.push(format!("{q}{simple_name}{q}: {value}"));
+            }
+            _ => {
+                continue;
+            }
         }
     }
 
