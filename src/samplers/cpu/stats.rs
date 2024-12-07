4af25b6bbd651d9cfad21c887fc9bea1342aac31
Brian Martin <brayniac@gmail.com>
2024-12-06 16:41:33 -0800

    cleanup

--- src/samplers/cpu/stats.rs
+++ src/samplers/cpu/stats.rs
@@ -38,61 +38,6 @@ pub static CPU_USAGE_NICE: LazyCounter = LazyCounter::new(Counter::default);
 )]
 pub static CPU_USAGE_SYSTEM: LazyCounter = LazyCounter::new(Counter::default);
 
-/// A function to format the cpu metrics that allows for export of both total
-/// and per-CPU metrics.
-///
-/// For the `Simple` format, the metrics will be formatted according to the
-/// a pattern which depends on the metric metadata:
-/// `{name}/cpu{id}` eg: `cpu/frequency/cpu0`
-/// `{name}/total` eg: `cpu/cycles/total`
-/// `{name}/{state}/cpu{id}` eg: `cpu/usage/user/cpu0`
-/// `{name}/{state}/total` eg: `cpu/usage/user/total`
-///
-/// For the `Prometheus` format, if the metric has an `id` set in the metadata,
-/// the metric name is left as-is. Otherwise, `/total` is appended. Note: we
-/// rely on the exposition logic to convert the `/`s to `_`s in the metric name.
-pub fn cpu_metric_formatter(metric: &MetricEntry, format: Format) -> String {
-    match format {
-        Format::Simple => {
-            let name = if let Some(state) = metric.metadata().get("state") {
-                format!("{}/{state}", metric.name())
-            } else {
-                metric.name().to_string()
-            };
-
-            if metric.metadata().contains_key("id") {
-                format!(
-                    "{name}/cpu{}",
-                    metric.metadata().get("id").unwrap_or("unknown"),
-                )
-            } else {
-                format!("{name}/total",)
-            }
-        }
-        Format::Prometheus => {
-            let metadata: Vec<String> = metric
-                .metadata()
-                .iter()
-                .map(|(key, value)| format!("{key}=\"{value}\""))
-                .collect();
-            let metadata = metadata.join(", ");
-
-            let name = if metric.metadata().contains_key("id") {
-                metric.name().to_string()
-            } else {
-                format!("{}/total", metric.name())
-            };
-
-            if metadata.is_empty() {
-                name
-            } else {
-                format!("{}{{{metadata}}}", name)
-            }
-        }
-        _ => metriken::default_formatter(metric, format),
-    }
-}
-
 pub fn cpu_usage_total_formatter(metric: &MetricEntry, format: Format) -> String {
     match format {
         Format::Simple => {
