20d0934fdae5b6691f11cedaf568f282da46a2e4
Brian Martin <brayniac@gmail.com>
2024-12-13 12:02:51 -0800

    fix

--- src/common/bpf/builder.rs
+++ src/common/bpf/builder.rs
@@ -56,7 +56,7 @@ pub struct Builder<T: 'static + SkelBuilder<'static>> {
     counters: Vec<(&'static str, Vec<&'static LazyCounter>)>,
     histograms: Vec<(&'static str, &'static RwLockHistogram)>,
     maps: Vec<(&'static str, Vec<u64>)>,
-    cpu_counters: Vec<(&'static str, ScopedCounters)>,
+    cpu_counters: Vec<(&'static str, Vec<&'static RwLockCounterGroup>)>,
     perf_events: Vec<(&'static str, PerfEvent)>,
     packed_counters: Vec<(&'static str, &'static RwLockCounterGroup)>,
 }
