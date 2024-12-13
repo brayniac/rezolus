fca2ca587dacb3074a5e31c7178547a95eb6362f
Brian Martin <brayniac@gmail.com>
2024-12-13 12:04:26 -0800

    drop formatters

--- src/samplers/cpu/linux/stats.rs
+++ src/samplers/cpu/linux/stats.rs
@@ -124,33 +124,3 @@ pub static CGROUP_CPU_MPERF: RwLockCounterGroup = RwLockCounterGroup::new(MAX_CG
 
 #[metric(name = "cgroup/cpu/tsc")]
 pub static CGROUP_CPU_TSC: RwLockCounterGroup = RwLockCounterGroup::new(MAX_CGROUPS);
-
-pub fn cpu_metric_percore_formatter(metric: &MetricEntry, format: Format) -> String {
-    match format {
-        Format::Simple => {
-            let id = metric
-                .metadata()
-                .get("id")
-                .expect("no `id` for metric formatter");
-            format!("{}/cpu{id}", metric.name())
-        }
-        _ => metric.name().to_string(),
-    }
-}
-
-pub fn cpu_usage_percore_formatter(metric: &MetricEntry, format: Format) -> String {
-    match format {
-        Format::Simple => {
-            let id = metric
-                .metadata()
-                .get("id")
-                .expect("no `id` for metric formatter");
-            let state = metric
-                .metadata()
-                .get("state")
-                .expect("no `state` for metric formatter");
-            format!("{}/{state}/cpu{id}", metric.name())
-        }
-        _ => metric.name().to_string(),
-    }
-}
