caa063c4e1c48eb9b7bbfd2aa45a2c41198eb21e
Brian Martin <brayniac@gmail.com>
2025-06-20 16:04:51 -0700

    fix

--- src/agent/samplers/network/linux/interfaces/mod.bpf.c
+++ src/agent/samplers/network/linux/interfaces/mod.bpf.c
@@ -25,7 +25,7 @@ int net_dev_xmit(struct trace_event_raw_net_dev_xmit *args)
 {
 	u32 offset = COUNTER_GROUP_WIDTH * bpf_get_smp_processor_id();
 
-	if args->rc != 0 {
+	if (args->rc != 0) {
 		u32 idx = offset;
 
 		array_incr(counters, offset);
